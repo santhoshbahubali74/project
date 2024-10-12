f620a9eae78d8b3f84e37dd1820bff8e75e35cfe
santhosh <144503376+santhoshbahubali74@users.noreply.github.com>
2024-10-12 16:01:01 +0530

    Create santhosh.py

---
+++ santhosh.py
@@ -0,0 +1,7 @@
+class ratan_sir:
+    def tata(tatamotor,tatasalt,tatavichle):
+        print("tatamotor",tatamotor)
+        print("tatasalt",tatasalt)
+        print("tatavichle",tatavichle)
+santhosh=ratan_sir
+santhosh.tata("jaguar","supersalt","hindiga")
